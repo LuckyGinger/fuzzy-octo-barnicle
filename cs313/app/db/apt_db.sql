d5961a41faa09d6451d2c42441f86ee0e539ad65
Thom <tallen.thom@gmail.com>
2016-05-15 17:03:05 -0600

    adding the file for my db

---
+++ cs313/app/db/apt_db.sql
@@ -0,0 +1,30 @@
+CREATE TABLE complex
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	description varchar(500) NOT NULL,
+	gender_housing boolean NOT NULL,
+	average_rating decimal(4, 2) NOT NULL,
+	semester_price int NOT NULL,
+	id PRIMARY KEY (id),
+)
+
+CREATE TABLE user_review
+(
+	id int NOT NULL AUTO_INCREMENT,
+	user_id int NOT NULL,
+	apt_id int NOT NULL,
+	review varchar(1000),
+	user_rating NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES user(id) NOT NULL,
+	FOREIGN KEY (apt_id) REFERENCES complex(id) NOT NULL
+)
+
+CREATE TABLE user
+(
+	id int NOT NULL AUTO_INCREMENT UNIQUE,
+	first_name varchar(80) NOT NULL,
+	last_name varchar(80) NOT NULL,
+	gender boolean NOT NULL,
+	PRIMARY KEY (id)
+)
